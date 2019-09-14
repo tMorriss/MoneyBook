7855de0ee0d603db0d164974d45e2706a52dcc1a
tMorriss <tmorriss@tmorriss.com>
2019-09-14 18:05:55 +0900

    add画面を足した

--- web/models.py
+++ web/models.py
@@ -9,6 +9,9 @@ class Direction(models.Model):
     def __str__(self):
         return self.name
 
+    def list():
+        return Direction.objects.order_by('pk')
+
 class Method(models.Model):
     order = models.IntegerField(default=0)
     name = models.CharField(max_length=200)
@@ -17,18 +20,26 @@ class Method(models.Model):
     def __str__(self):
         return self.name
 
+    def list():
+        return Method.objects.filter(order__gt='0').order_by('order')
+
 class Genre(models.Model):
     name = models.CharField(max_length=10)
 
     def __str__(self):
         return self.name
 
+    def first_list():
+        return Genre.objects.filter(pk__gt='0').order_by('pk')
+    def latter_list():
+        return Genre.objects.filter(pk__lte='0').order_by('-pk')
+
 class Data(models.Model):
     date = models.DateField()
     item = models.CharField(max_length=100)
     price = models.IntegerField()
-    method = models.ForeignKey(Method, on_delete=models.CASCADE)
     direction = models.ForeignKey(Direction, on_delete=models.CASCADE)
+    method = models.ForeignKey(Method, on_delete=models.CASCADE)
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
     temp = models.BooleanField()
     checked = models.BooleanField()
@@ -36,13 +47,14 @@ class Data(models.Model):
     def __str__(self):
         return self.item
 
-    def monthData(year, month):
+    def getMonthData(year, month):
         y = str(year)
         m = '{0:02d}'.format(month)
         start = y + "-" + m + "-1"
         end = y + "-" + m + "-" + str(calendar.monthrange(year, month)[1])
         return Data.objects.filter(date__range=(start, end))
 
-    def toMonthData():
+    def getToMonthData():
         today = datetime.today()
-        return Data.monthData(today.year, today.month)
+        return Data.getMonthData(today.year, today.month)
+
--- web/views.py
+++ web/views.py
@@ -3,14 +3,34 @@ from django.http import HttpResponse
 from .models import *
 from django.template import loader
 from django.conf import settings
+from datetime import datetime
 
 def index(request):
     if "year" in request.GET and "month" in request.GET:
-        monthlyData = Data.monthData(int(request.GET.get("year")), int(request.GET.get("month")))
+        monthlyData = Data.getMonthData(int(request.GET.get("year")), int(request.GET.get("month")))
     else:
-        monthlyData = Data.toMonthData()
+        monthlyData = Data.getToMonthData()
     content = {
         'monthly_data': monthlyData,
-        'app_name': settings.APP_NAME
+        'app_name': settings.APP_NAME,
     }
     return render(request, 'web/index.html', content)
+
+def add(request):
+    now = datetime.now()
+    directions = Direction.list()
+    methods = Method.list()
+    first_genres = Genre.first_list()
+    latter_genres = Genre.latter_list()
+    temps = {0:"No", 1:"Yes"}
+    content = {
+        'app_name': settings.APP_NAME,
+        'year': now.year,
+        'month': now.month,
+        'directions': directions,
+        'methods': methods,
+        'first_genres': first_genres,
+        'latter_genres': latter_genres,
+        'temps': temps,
+    }
+    return render(request, 'web/add.html', content)
