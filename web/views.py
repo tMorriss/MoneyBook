605253cd12a5f89a13d9f35c3ff7caa20aa88042
tMorriss <tmorriss@tmorriss.com>
2019-09-14 15:40:20 +0900

    headerを追加

--- web/views.py
+++ web/views.py
@@ -2,16 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
 from django.template import loader
-
-# Create your views here.
+from django.conf import settings
 
 def index(request):
     if "year" in request.GET and "month" in request.GET:
         monthlyData = Data.monthData(int(request.GET.get("year")), int(request.GET.get("month")))
     else:
         monthlyData = Data.toMonthData()
-    # allData = Data.objects.all()
-    content = {'monthly_data': monthlyData}
+    content = {
+        'monthly_data': monthlyData,
+        'app_name': settings.APP_NAME
+    }
     return render(request, 'web/index.html', content)
-    # template = loader.get_template('web/index.html')
-    # return HttpResponse(template.render(content, request))
