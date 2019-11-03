ad90d5da36c8b02c3fa8e72176b6d6490fddf95c
tMorriss <tmorriss@tmorriss.com>
2019-11-03 14:27:03 +0900

    プロジェクト、アプリ名を変更

--- web/middleware/auth.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponseRedirect
-from django.utils.deprecation import MiddlewareMixin
-from django.urls import reverse
-
-class authMiddleware(MiddlewareMixin):
-    def process_response(self, request, response):
-        if not request.user.is_authenticated and request.path != reverse('web:login'):
-            return HttpResponseRedirect(reverse('web:login'))
-        return response
