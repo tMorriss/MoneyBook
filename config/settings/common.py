ddec6114404bd4c0f010a90356addc4f48f2d650
tMorriss <tmorriss@tmorriss.com>
2019-11-10 18:44:45 +0900

    いらなそうなmiddlewareを削除

--- config/settings/common.py
+++ config/settings/common.py
@@ -49,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'moneybook.middleware.auth.authMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
