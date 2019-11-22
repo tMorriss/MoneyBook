a548e22bdb102fc8a7b80604ebeb5a1e8a4e6dac
tMorriss <tmorriss@tmorriss.com>
2019-11-22 22:48:02 +0900

    Genreにshow_orderを追加 pkを1以上に

--- createDateYaml.py
+++ createDateYaml.py
@@ -6,6 +6,19 @@ query = "SELECT * FROM data ORDER BY id"
 result = pd.read_sql(query, con)
 resultStr = ""
 for iRow in range(len(result)):
+    
+    genre = result.iat[iRow, 6]
+    if genre == 0:
+        genre = 8
+    elif genre == -1:
+        genre = 9
+    elif genre == -2:
+        genre = 10
+    elif genre == -3:
+        genre = 11
+    elif genre == -4:
+        genre = 12
+
     resultStr += "- model: moneybook.data\n"
     resultStr += "  pk: " + str(iRow + 1) + "\n"
     resultStr += "  fields: \n"
@@ -13,8 +26,8 @@ for iRow in range(len(result)):
     resultStr += "    item: " + str(result.iat[iRow, 2]).replace("amp;","") + "\n"
     resultStr += "    price: " + str(result.iat[iRow, 3]) + "\n"
     resultStr += "    method: " + str(result.iat[iRow, 4]) + "\n"
-    resultStr += "    direction: " + str(result.iat[iRow, 5]) + "\n"
-    resultStr += "    genre: " + str(result.iat[iRow, 6]) + "\n"
+    resultStr += "    direction: " + str(result.iat[iRow, 5] + 1) + "\n"
+    resultStr += "    genre: " + str(genre) + "\n"
     resultStr += "    temp: " + str(result.iat[iRow, 7]) + "\n"
     resultStr += "    checked: " + str(result.iat[iRow, 8]) + "\n"
 print(resultStr)
