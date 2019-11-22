0ed41abe058f2114493d801e2cfabe1b2c9eef17
tMorriss <tmorriss@tmorriss.com>
2019-11-22 21:04:34 +0900

    prodの設定にmariadbを追加

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,1 +1,12 @@
 from .common import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'moneybook_django',
+        'USER': 'moneybook',
+        'PASSWORD': 'want2money',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
