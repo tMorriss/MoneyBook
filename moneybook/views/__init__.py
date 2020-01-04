e7b510ae8b52c74d2db0fa2e6f9ad7cc8e3822d5
tMorriss <tmorriss@tmorriss.com>
2020-01-04 15:51:05 +0900

    CustomLoginView.pyをmyCustomLoginView.pyにrename

--- moneybook/views/__init__.py
+++ moneybook/views/__init__.py
@@ -6,5 +6,5 @@ from .searchView import *
 from .toolsView import *
 from .editView import *
 from .deleteView import *
-from .CustomLoginView import *
+from .myCustomLoginView import *
 from .paypayView import *
