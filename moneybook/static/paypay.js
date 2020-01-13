b6f9e786358c20e941c529350946b7aa7cdeb052
tMorriss <tmorriss@tmorriss.com>
2020-01-13 23:25:52 +0900

    /paypayの残高をajax化。キャッシュバック追加で更新するように

--- moneybook/static/paypay.js
+++ moneybook/static/paypay.js
@@ -1,3 +1,6 @@
+function separate(num){
+    return String(num).replace( /(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
+}
 function get_transaction() {
     $.ajax({
         url: data_table_url,
@@ -27,6 +30,7 @@ function send_paypay_cacheback() {
     // 成功時
     .done(() => {
         show_result_msg("Success!", get_transaction);
+        get_payapy_balance();
         reset_form();
     })
     // 失敗時
@@ -62,6 +66,16 @@ function key_press_cacheback(code) {
     if (code === 13) {
         update_cacheback_date();
     }
+}
+
+function get_payapy_balance() {
+    $.get({
+        url: paypay_balance_url
+    })
+    .done((data) => {
+        result = JSON.parse(data);
+        $("#paypay_balance").text(separate(result["balance"]) + "円");
+    });
 }
 
 function reset_form() {
