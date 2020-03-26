a771828abcb4ed970b9373c3d4c1c3866d4135bf
tMorriss <tmorriss@tmorriss.com>
2020-03-26 21:49:15 +0900

    誤字修正

--- moneybook/static/edit.js
+++ moneybook/static/edit.js
@@ -5,7 +5,7 @@ function send_update_row() {
             "csrfmiddlewaretoken": $('input[name="csrfmiddlewaretoken"]').val(),
             "date": $('#year').val() + "-" + $('#month').val() + "-" + $('#day').val(),
             "item": $('#item').val(),
-            "price": remoteComma($('#price').val()),
+            "price": removeComma($('#price').val()),
             "direction": $('input[name="direction"]:checked').val(),
             "method": $('input[name="method"]:checked').val(),
             "genre": $('input[name="genre"]:checked').val(),
