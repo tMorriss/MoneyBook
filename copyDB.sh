134e9984d496b75dd475c441a7ca4df5c9d263a9
tMorriss <tmorriss@tmorriss.com>
2020-03-29 23:04:50 +0900

    copyスクリプトのコピー元をneptuneに変更

--- copyDB.sh
+++ copyDB.sh
@@ -6,10 +6,10 @@ cd `dirname $0`
 # python3 manage.py makemigrations
 # python3 manage.py migrate
 
-scp createDataYaml.py createOtherYaml.py tmorriss:~/
+scp createDataYaml.py createOtherYaml.py neptune:~/
 
-ssh tmorriss python3 /home/tmorriss/createDataYaml.py > fixture/data_all.yaml
-ssh tmorriss python3 /home/tmorriss/createOtherYaml.py > fixture/initial_data.yaml
+ssh neptune python3 /home/tmorriss/createDataYaml.py > fixture/data_all.yaml
+ssh neptune python3 /home/tmorriss/createOtherYaml.py > fixture/initial_data.yaml
 
 python3 manage.py loaddata fixture/initial_data.yaml
 python3 manage.py loaddata fixture/data_all.yaml
