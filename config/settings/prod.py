1be5a7086cdd59ae5cdcfacd13cb002a88671ed2
tMorriss <tmorriss@tmorriss.com>
2020-04-25 18:06:13 +0900

    再び本番DBを秘匿。本番のallows_hostsを*に変更
    This reverts commit 75470896b37c71659acaa152965c1718fc60fccd.

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,16 +1,17 @@
 from .common import *
+import os
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'moneybook_django',
-        'USER': 'moneybook',
-        'PASSWORD': 'want2money',
-        'HOST': 'neptune.tmorriss.com',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST'),
         'PORT': '3306',
     }
 }
 
 APP_NAME = "MoneyBook"
 
-ALLOWED_HOSTS = ['moneybook.tmorriss.com']
+ALLOWED_HOSTS = ['*']
