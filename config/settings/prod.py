271b47bfe6869bc6e77e8d7733caf4e63c792286
tMorriss <tmorriss@tmorriss.com>
2020-05-30 18:59:48 +0900

    prod.pyを履歴ごと削除したので新しく追加

---
+++ config/settings/prod.py
@@ -0,0 +1,17 @@
+from .common import *
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': '3306',
+    }
+}
+
+APP_NAME = "MoneyBook"
+
+ALLOWED_HOSTS = ['*']
