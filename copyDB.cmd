6cd7156259b490192a45f9143b05ab6acb469b49
tMorriss <tmorriss@tmorriss.com>
2020-06-06 18:02:47 +0900

    fix copyDB scripts

--- copyDB.cmd
+++ copyDB.cmd
@@ -4,6 +4,8 @@ cd %~dp0
 
 scp createDataYaml.py createOtherYaml.py neptune:~/
 
+mkdir fixture
+
 ssh neptune python3 /home/tmorriss/createDataYaml.py > fixture\data_all.yaml
 ssh neptune python3 /home/tmorriss/createOtherYaml.py > fixture\initial_data.yaml
 
--- copyDB.sh
+++ copyDB.sh
@@ -8,6 +8,8 @@ cd `dirname $0`
 
 scp createDataYaml.py createOtherYaml.py neptune:~/
 
+mkdir -p fixture
+
 ssh neptune python3 /home/tmorriss/createDataYaml.py > fixture/data_all.yaml
 ssh neptune python3 /home/tmorriss/createOtherYaml.py > fixture/initial_data.yaml
 
