27a1e89b7ccfa93f7f865a1249af93c0eee310bc
tMorriss <13708349+tMorriss@users.noreply.github.com>
2024-08-03 15:36:42 +0900

    fix server

--- copyDB.cmd
+++ copyDB.cmd
@@ -2,12 +2,12 @@
 
 cd %~dp0
 
-scp createDataYaml.py createOtherYaml.py neptune:~/
+scp createDataYaml.py createOtherYaml.py mars:~/
 
 mkdir fixture
 
-ssh neptune python3 /home/tmorriss/createDataYaml.py > fixture\data_all.yaml
-ssh neptune python3 /home/tmorriss/createOtherYaml.py > fixture\initial_data.yaml
+ssh mars python3 /home/tmorriss/createDataYaml.py > fixture\data_all.yaml
+ssh mars python3 /home/tmorriss/createOtherYaml.py > fixture\initial_data.yaml
 
 python manage.py loaddata fixture\initial_data.yaml
 python manage.py loaddata fixture\data_all.yaml
--- copyDB.sh
+++ copyDB.sh
@@ -6,12 +6,12 @@ cd `dirname $0`
 # python3 manage.py makemigrations
 # python3 manage.py migrate
 
-scp createDataYaml.py createOtherYaml.py neptune:~/
+scp createDataYaml.py createOtherYaml.py mars:~/
 
 mkdir -p fixture
 
-ssh neptune python3 /home/tmorriss/createDataYaml.py > fixture/data_all.yaml
-ssh neptune python3 /home/tmorriss/createOtherYaml.py > fixture/initial_data.yaml
+ssh mars python3 /home/tmorriss/createDataYaml.py > fixture/data_all.yaml
+ssh mars python3 /home/tmorriss/createOtherYaml.py > fixture/initial_data.yaml
 
 python3 manage.py loaddata fixture/initial_data.yaml
 python3 manage.py loaddata fixture/data_all.yaml
