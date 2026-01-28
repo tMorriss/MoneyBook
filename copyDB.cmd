11f912635c186494008442abf5fa848a7f5e31b7
tmorriss <13708349+tMorriss@users.noreply.github.com>
2026-01-29 00:31:21 +0900

    データベース接続情報を1passwordから取得するようにした

--- copyDB.cmd
+++ copyDB.cmd
@@ -6,8 +6,14 @@ scp createDataYaml.py createOtherYaml.py mars:~/
 
 mkdir fixture
 
-ssh mars python3 /home/tmorriss/createDataYaml.py > fixture\data_all.yaml
-ssh mars python3 /home/tmorriss/createOtherYaml.py > fixture\initial_data.yaml
+for /f "delims=" %%i in ('op read "op://Personal/MoneyBook_DB/hostname"') do set DB_HOSTNAME=%%i
+for /f "delims=" %%i in ('op read "op://Personal/MoneyBook_DB/port"') do set DB_PORT=%%i
+for /f "delims=" %%i in ('op read "op://Personal/MoneyBook_DB/username"') do set DB_USER=%%i
+for /f "delims=" %%i in ('op read "op://Personal/MoneyBook_DB/password"') do set DB_PASSWORD=%%i
+for /f "delims=" %%i in ('op read "op://Personal/MoneyBook_DB/database"') do set DB_DATABASE=%%i
+
+ssh mars python3 /home/tmorriss/createDataYaml.py "%DB_HOSTNAME%" "%DB_PORT%" "%DB_USER%" "%DB_PASSWORD%" "%DB_DATABASE%" > fixture\data_all.yaml
+ssh mars python3 /home/tmorriss/createOtherYaml.py "%DB_HOSTNAME%" "%DB_PORT%" "%DB_USER%" "%DB_PASSWORD%" "%DB_DATABASE%" > fixture\initial_data.yaml
 
 python manage.py loaddata fixture\initial_data.yaml
 python manage.py loaddata fixture\data_all.yaml
