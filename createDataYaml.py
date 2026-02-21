611954068f4cf3e3c6a841650258e44c7937788b
tMorriss <13708349+tMorriss@users.noreply.github.com>
2026-02-21 19:13:44 +0900

    Refactor YAML creation scripts to use PyYAML library

--- createDataYaml.py
+++ createDataYaml.py
@@ -1,22 +1,27 @@
 import pandas as pd
+import yaml
 from yaml_utils import create_db_connection, parse_db_credentials_from_stdin
 
 db_host, db_port, db_user, db_password, db_database = parse_db_credentials_from_stdin()
 con = create_db_connection(db_host, db_port, db_user, db_password, db_database)
 query = 'SELECT * FROM moneybook_data ORDER BY id'
 result = pd.read_sql(query, con)
-result_str = ''
-for i_row in range(len(result)):
 
-    result_str += '- model: moneybook.Data\n'
-    result_str += '  pk: ' + str(i_row + 1) + '\n'
-    result_str += '  fields: \n'
-    result_str += '    date: ' + str(result.iat[i_row, 1]) + '\n'
-    result_str += '    item: ' + str(result.iat[i_row, 2]).replace('amp;', '') + '\n'
-    result_str += '    price: ' + str(result.iat[i_row, 3]) + '\n'
-    result_str += '    temp: ' + str(result.iat[i_row, 4]) + '\n'
-    result_str += '    checked: ' + str(result.iat[i_row, 5]) + '\n'
-    result_str += '    direction_id: ' + str(result.iat[i_row, 6]) + '\n'
-    result_str += '    category_id: ' + str(result.iat[i_row, 7]) + '\n'
-    result_str += '    method_id: ' + str(result.iat[i_row, 8]) + '\n'
-print(result_str)
+data = []
+for i_row in range(len(result)):
+    data.append({
+        'model': 'moneybook.Data',
+        'pk': i_row + 1,
+        'fields': {
+            'date': str(result.iat[i_row, 1]),
+            'item': str(result.iat[i_row, 2]).replace('amp;', ''),
+            'price': int(result.iat[i_row, 3]),
+            'temp': bool(result.iat[i_row, 4]),
+            'checked': bool(result.iat[i_row, 5]),
+            'direction_id': int(result.iat[i_row, 6]),
+            'category_id': int(result.iat[i_row, 7]),
+            'method_id': int(result.iat[i_row, 8]),
+        }
+    })
+
+print(yaml.dump(data, allow_unicode=True, sort_keys=False))
--- createOtherYaml.py
+++ createOtherYaml.py
@@ -1,90 +1,105 @@
 import pandas as pd
+import yaml
 from yaml_utils import create_db_connection, parse_db_credentials_from_stdin
 
 db_host, db_port, db_user, db_password, db_database = parse_db_credentials_from_stdin()
 con = create_db_connection(db_host, db_port, db_user, db_password, db_database)
-result_str = ''
+data = []
 
 # 現在銀行
 query = 'SELECT * FROM moneybook_bankbalance ORDER BY id'
 result = pd.read_sql(query, con)
 for i_row in range(len(result)):
-    result_str += '- model: moneybook.BankBalance\n'
-    result_str += '  pk: ' + str(result.iat[i_row, 0]) + '\n'
-    result_str += '  fields:\n'
-    result_str += '    name: ' + str(result.iat[i_row, 1]) + '\n'
-    result_str += '    price: ' + str(result.iat[i_row, 2]) + '\n'
-    result_str += '    show_order: ' + str(result.iat[i_row, 3]) + '\n'
-result_str += '\n'
+    data.append({
+        'model': 'moneybook.BankBalance',
+        'pk': int(result.iat[i_row, 0]),
+        'fields': {
+            'name': str(result.iat[i_row, 1]),
+            'price': int(result.iat[i_row, 2]),
+            'show_order': int(result.iat[i_row, 3]),
+        }
+    })
 
 # チェック
 query = 'SELECT * FROM moneybook_checkeddate ORDER BY id'
 result = pd.read_sql(query, con)
 for i_row in range(len(result)):
-    result_str += '- model: moneybook.CheckedDate\n'
-    result_str += '  pk: ' + str(result.iat[i_row, 0]) + '\n'
-    result_str += '  fields:\n'
-    result_str += '    date: ' + str(result.iat[i_row, 1]) + '\n'
-    result_str += '    method_id: ' + str(result.iat[i_row, 2]) + '\n'
-result_str += '\n'
+    data.append({
+        'model': 'moneybook.CheckedDate',
+        'pk': int(result.iat[i_row, 0]),
+        'fields': {
+            'date': str(result.iat[i_row, 1]),
+            'method_id': int(result.iat[i_row, 2]),
+        }
+    })
 
 # クレジットチェック
 query = 'SELECT * FROM moneybook_creditcheckeddate ORDER BY id'
 result = pd.read_sql(query, con)
 for i_row in range(len(result)):
-    result_str += '- model: moneybook.CreditCheckedDate\n'
-    result_str += '  pk: ' + str(result.iat[i_row, 0]) + '\n'
-    result_str += '  fields:\n'
-    result_str += '    name: ' + str(result.iat[i_row, 1]) + '\n'
-    result_str += '    date: ' + str(result.iat[i_row, 2]) + '\n'
-    result_str += '    price: ' + str(result.iat[i_row, 3]) + '\n'
-    result_str += '    show_order: ' + str(result.iat[i_row, 3]) + '\n'
-result_str += '\n'
+    data.append({
+        'model': 'moneybook.CreditCheckedDate',
+        'pk': int(result.iat[i_row, 0]),
+        'fields': {
+            'name': str(result.iat[i_row, 1]),
+            'date': str(result.iat[i_row, 2]),
+            'price': int(result.iat[i_row, 3]),
+            'show_order': int(result.iat[i_row, 4]),
+        }
+    })
 
-# クレジットチェック
+# 方向（Direction）
 query = 'SELECT * FROM moneybook_direction ORDER BY id'
 result = pd.read_sql(query, con)
 for i_row in range(len(result)):
-    result_str += '- model: moneybook.Direction\n'
-    result_str += '  pk: ' + str(result.iat[i_row, 0]) + '\n'
-    result_str += '  fields:\n'
-    result_str += '    name: ' + str(result.iat[i_row, 1]) + '\n'
-result_str += '\n'
+    data.append({
+        'model': 'moneybook.Direction',
+        'pk': int(result.iat[i_row, 0]),
+        'fields': {
+            'name': str(result.iat[i_row, 1]),
+        }
+    })
 
 # ジャンル
 query = 'SELECT * FROM moneybook_category ORDER BY id'
 result = pd.read_sql(query, con)
 for i_row in range(len(result)):
-    result_str += '- model: moneybook.Category\n'
-    result_str += '  pk: ' + str(result.iat[i_row, 0]) + '\n'
-    result_str += '  fields:\n'
-    result_str += '    name: ' + str(result.iat[i_row, 1]) + '\n'
-    result_str += '    show_order: ' + str(result.iat[i_row, 2]) + '\n'
-    result_str += '    is_living_cost: ' + str(result.iat[i_row, 3]) + '\n'
-    result_str += '    is_variable_cost: ' + str(result.iat[i_row, 4]) + '\n'
-result_str += '\n'
+    data.append({
+        'model': 'moneybook.Category',
+        'pk': int(result.iat[i_row, 0]),
+        'fields': {
+            'name': str(result.iat[i_row, 1]),
+            'show_order': int(result.iat[i_row, 2]),
+            'is_living_cost': bool(result.iat[i_row, 3]),
+            'is_variable_cost': bool(result.iat[i_row, 4]),
+        }
+    })
 
 # 支払い方法
 query = 'SELECT * FROM moneybook_method ORDER BY id'
 result = pd.read_sql(query, con)
 for i_row in range(len(result)):
-    result_str += '- model: moneybook.Method\n'
-    result_str += '  pk: ' + str(result.iat[i_row, 0]) + '\n'
-    result_str += '  fields:\n'
-    result_str += '    show_order: ' + str(result.iat[i_row, 1]) + '\n'
-    result_str += '    name: ' + str(result.iat[i_row, 2]) + '\n'
-    result_str += '    chargeable: ' + str(result.iat[i_row, 3]) + '\n'
-result_str += '\n'
+    data.append({
+        'model': 'moneybook.Method',
+        'pk': int(result.iat[i_row, 0]),
+        'fields': {
+            'show_order': int(result.iat[i_row, 1]),
+            'name': str(result.iat[i_row, 2]),
+            'chargeable': bool(result.iat[i_row, 3]),
+        }
+    })
 
 # 諸々の価格
 query = 'SELECT * FROM moneybook_severalcosts ORDER BY id'
 result = pd.read_sql(query, con)
 for i_row in range(len(result)):
-    result_str += '- model: moneybook.SeveralCosts\n'
-    result_str += '  pk: ' + str(result.iat[i_row, 0]) + '\n'
-    result_str += '  fields:\n'
-    result_str += '    name: ' + str(result.iat[i_row, 1]) + '\n'
-    result_str += '    price: ' + str(result.iat[i_row, 2]) + '\n'
-result_str += '\n'
+    data.append({
+        'model': 'moneybook.SeveralCosts',
+        'pk': int(result.iat[i_row, 0]),
+        'fields': {
+            'name': str(result.iat[i_row, 1]),
+            'price': int(result.iat[i_row, 2]),
+        }
+    })
 
-print(result_str)
+print(yaml.dump(data, allow_unicode=True, sort_keys=False))
